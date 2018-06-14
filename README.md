60bf8301a3b9638a514620050540759a3068b2e6
saltgamer <saltgamer@gmail.com>
2018-06-14 13:10:38 +0900

    second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# musicNote
+
+webTextBook prototype
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+/***
+ * file name : webpack.config.js
+ * description : webcade webpack config
+ * create date : 2018-04-24
+ * creator : saltgamer
+ ***/
+
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // "production" | "development" | "none"
+    entry: ['babel-polyfill', 'whatwg-fetch', './src/js/main.js'],
+    output: {
+        filename: 'webTextBook.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.json$/,
+                use: ['json-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images/'
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['env', {
+                                targets: {
+                                    browsers: ['last 2 versions']
+                                }
+                            }]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    optimization: {},
+    resolve: {
+        modules: [
+            'node_modules'
+        ],
+        extensions: ['.js', '.json', '.css']
+    }
+};
